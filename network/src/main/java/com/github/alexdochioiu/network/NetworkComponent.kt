0eb2013cc6b4d29dcdc8b2669c71f2a3a6834d3a
DESKTOP-FE3E8CL-JeeFo <dochioiu.alex@gmail.com>
2019-03-21 14:43:56 +0000

    Some initial setup

---
+++ network/src/main/java/com/github/alexdochioiu/network/NetworkComponent.kt
@@ -0,0 +1,10 @@
+package com.github.alexdochioiu.network
+
+import dagger.Component
+
+/**
+ * Created by Alexandru Iustin Dochioiu on 21-Mar-19
+ */
+@Component
+@NetworkScope
+interface NetworkComponent
---
+++ network/src/main/java/com/github/alexdochioiu/network/NetworkScope.kt
@@ -0,0 +1,10 @@
+package com.github.alexdochioiu.network
+
+import javax.inject.Scope
+
+/**
+ * Created by Alexandru Iustin Dochioiu on 21-Mar-19
+ */
+@Retention(AnnotationRetention.SOURCE)
+@Scope
+annotation class NetworkScope
